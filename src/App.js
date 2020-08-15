af738cdae4072b66004e08eece0c11ab2b68cddb
musicprogram <jahenao17@misena.edu.co>
2020-08-15 10:21:45 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import ActionCable from 'actioncable'
+
+class App extends Component {
+  state = { body: '' }
+
+  componentDidMount() {
+    window.fetch('http://localhost:3001/messages/1').then(data => {
+      data.json().then(res => {
+        this.setState({ body: res.body })
+      })
+    })
+
+    const cable = ActionCable.createConsumer('ws://localhost:3001/cable')
+    this.sub = cable.subscriptions.create('MessagesChannel', {
+      received: this.handleReceiveNewText
+    })
+  }
+
+  handleReceiveNewText = ({ body }) => {
+    if (body !== this.state.body) {
+      this.setState({ body })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title">
+          ShareYourThink
+        </h1>
+        <CKEditor
+          editor={ ClassicEditor }
+          config={{
+            toolbar: ['heading', '|', 'bold', 'italic', 'blockQuote', 'link', 'numberedList', 'bulletedList', 'insertTable',
+              'tableColumn', 'tableRow', 'mergeTableCells', 'mediaEmbed', '|', 'undo', 'redo' ]
+          }}
+          data={this.state.body}
+          onInit={ editor => {
+            // You can store the "editor" and use when it is needed.
+            //console.log( 'Editor is ready to use!', editor );
+          } }
+          onChange={ ( event, editor ) => {
+            const data = editor.getData();
+            // console.log( { event, editor, data } );
+            this.setState({ body: data })
+            this.sub.send({ body: data, id: 1 })
+          } }
+          onBlur={ ( event, editor ) => {
+            // console.log( 'Blur.', editor );
+          } }
+          onFocus={ ( event, editor ) => {
+            // console.log( 'Focus.', editor );
+          } }
+        />
+
+      </div>
+
+    )
+  }
 }
 
 export default App;
